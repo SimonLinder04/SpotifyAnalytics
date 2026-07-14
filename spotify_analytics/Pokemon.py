4558d64ebcc40e912540b557f9d2de02f3a84086
Simon Linder <simon.karl@live.se>
2026-07-14 15:39:29 +0200

    added a printing implementation

--- spotify_analytics/Pokemon.py
+++ spotify_analytics/Pokemon.py
@@ -1,7 +1,10 @@
 import requests
+from pprint import pprint
 
 base_url = "https://pokeapi.co/api/v2/"
 pokemon_name = "pikachu"
+
+
 
 def get_pokemon_info(name):
     url = f"{base_url}/pokemon/{pokemon_name}"
@@ -17,5 +20,30 @@ def get_pokemon_info(name):
 
 
 
+def print_data(data, depth=0, max_depth=2):
+    indentation = "    " * depth
 
-print(get_pokemon_info(pokemon_name)["height"])
+    if isinstance(data, dict):
+        for key, value in data.items():
+            print(indentation + str(key))
+
+            if isinstance(value, (dict, list)):
+                if depth < max_depth - 1:
+                    print_data(value, depth + 1, max_depth)
+            else:
+                if depth < max_depth - 1:
+                    print("    " * (depth + 1) + str(value))
+
+    elif isinstance(data, list):
+        for item in data:
+            if depth < max_depth:
+                print_data(item, depth, max_depth)
+
+    else:
+        print(indentation + str(data))
+
+
+print(f"{"-" * 100}\n" *10)
+
+pokemon_info = get_pokemon_info(pokemon_name)
+print_data(pokemon_info)
